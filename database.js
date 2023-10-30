fffa5a9460e4fe98cf91f3ac046092db55148cf2
GittyRabinowitz <g0533194100@gmail.com>
2023-10-30 15:01:23 +0200

    a commit

--- database.js
+++ database.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const MONGODB_URI = 'mongodb://mongo-db:27017';
 
+
 mongoose.connect(MONGODB_URI,{useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
   .then(() => {
     console.log("Successfully connected to MongoDB.");
